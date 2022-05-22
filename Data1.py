f9fc9ca14b428b299d0bc559ab5acd42c1a9194d
GraceEnsign <105288631+GraceEnsign@users.noreply.github.com>
2022-05-22 17:49:08 -0400

    Second commit

--- Data1.py
+++ Data1.py
@@ -1,1 +1,10 @@
 print("Hello World!")
+
+Capitals = ['Albany', 'Tallahassee', 'Frankfort', 'Sacramento']
+print(Capitals[3])
+
+State_Capitals = {'New York':'Albany', 'California':'Sacramento'}
+print(State_Capitals['New York'])
+
+States = ('New York', 'Florida', 'Kentucky', 'California')
+print(States[2])
